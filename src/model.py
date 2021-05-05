d685007f211371389ff075b8ea7685a7d29e0655
trtd56 <5toda6@gmail.com>
2021-05-05 10:22:03 +0900

    get n_labels

--- src/model.py
+++ src/model.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 from transformers import *
 
 class WrimeBert(nn.Module):
-    def __init__(self, model_type, tokenizer):
+    def __init__(self, model_type, tokenizer, n_label):
         super(WrimeBert, self).__init__()
 
         bert_conf = BertConfig(model_type)
         bert_conf.vocab_size = tokenizer.vocab_size
 
         self.bert = AutoModel.from_pretrained(model_type, config=bert_conf)
-        self.fc = nn.Linear(bert_conf.hidden_size, 1)
+        self.fc = nn.Linear(bert_conf.hidden_size, n_label)
 
 
     def forward(self, ids, mask, token_type_ids):
